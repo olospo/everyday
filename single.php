f2c7a7e83bdad0b55aa7477c4cbd0e653e8b6ef2
Tom Brooks <tom@olospo.co.uk>
2025-01-23 16:08:14 +0000

    Single update

--- single.php
+++ single.php
@@ -13,9 +13,13 @@ while ( have_posts() ) : the_post(); ?>
 
 <section class="post news">
   <div class="container flex">
-    <div class="content twelve columns">
+    <div class="content eight columns">
       <?php the_content(); ?>
     </div>
+    <aside class="four columns">
+      <h3>UX insights in your inbox</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </aside>
   </div>
 </section>
 
