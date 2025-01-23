37d4be6f5373e4f14d4e6349382d8f77ab77e767
Tom Brooks <tom@olospo.co.uk>
2025-01-23 15:12:50 +0000

    About page

--- inc/profile.php
+++ inc/profile.php
@@ -4,11 +4,13 @@ $post = get_sub_field('profile');
 setup_postdata($post); ?>
 <article class="profile">
   <div class="image three columns">
-    <img src="<?php the_post_thumbnail_url( 'large-thumb' ); ?>" alt="<?php the_title(); ?>" ?>
+    <img src="<?php the_post_thumbnail_url( 'large-thumb' ); ?>" alt="<?php the_title(); ?>">
   </div>
   <div class="content nine columns">
-    <h4><?php the_title(); ?></h4>
+    <h3><?php the_title(); ?></h3>
     <?php the_content(); ?>
   </div>
 </article>
 <?php wp_reset_postdata(); ?>
+
+<img src="">
--- templates/about.php
+++ templates/about.php
@@ -5,7 +5,7 @@ $teamHeading = get_field('heading');
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
-<section class="home hero">
+<section class="about hero">
   <div class="container">
     <div class="content six columns">
       <h1>Since 2012,  we’ve helped big and small teams design products that impact people’s lives.</h1>
@@ -13,16 +13,43 @@ get_header(); ?>
   </div>  
 </section>
 
-<section class="home team">
+<section class="office">
+  <div class="container">
+    <div class="twelve columns">
+      <img src="<?php bloginfo('template_directory'); ?>/img/office.jpg" alt="Everyday Office">
+      <span>Our office in Atlanta</span>
+    </div>
+  </div>
+</section>
+
+<section class="what">
+  <div class="container">
+    <div class="ten columns offset-by-one">
+    <h2>What we do</h2>
+    <p>We’re based in Atlanta, but we partner with clients across the globe.  We work with product teams to help them learn about their customers, create concepts, test and refine those concepts, then deliver final designs.</p>
+    </div>
+  </div>
+</section>
+
+<section class="team">
   <div class="container">
     <div class="content twelve columns">
       <h2><?php echo $teamHeading; ?></h2>
-
       <?php if (have_rows('team_members')) { // Flexible Content ?>
         <?php while (have_rows('team_members')) { the_row(); ?>
           <?php get_template_part( 'inc/profile' ); ?>
         <?php } ?>
       <?php } ?>
+      <article class="joinus">
+        <div class="image three columns">
+          <img src="<?php bloginfo('template_directory'); ?>/img/joinus-bg.jpg" >
+        </div>
+        <div class="content nine columns">
+          <h3>Join us</h3>
+          <p>Interested in joining our team?</p>
+          <a href="<?php echo get_site_url(); ?>/careers">View roles</a>
+        </div>
+      </article>
     </div>
   </div>  
 </section>
