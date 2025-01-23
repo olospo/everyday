5fc5fe86cc6b4637f0c3e9a918c40e20c6fb340a
Tom Brooks <tom@olospo.co.uk>
2025-01-23 14:43:12 +0000

    About page update

--- templates/about.php
+++ templates/about.php
@@ -1,4 +1,6 @@
 <?php /* Template Name: About */
+  
+$teamHeading = get_field('heading');
 
 get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
@@ -7,6 +9,20 @@ get_header(); ?>
   <div class="container">
     <div class="content six columns">
       <h1>Since 2012,  we’ve helped big and small teams design products that impact people’s lives.</h1>
+    </div>
+  </div>  
+</section>
+
+<section class="home team">
+  <div class="container">
+    <div class="content twelve columns">
+      <h2><?php echo $teamHeading; ?></h2>
+
+      <?php if (have_rows('team_members')) { // Flexible Content ?>
+        <?php while (have_rows('team_members')) { the_row(); ?>
+          <?php get_template_part( 'inc/profile' ); ?>
+        <?php } ?>
+      <?php } ?>
     </div>
   </div>  
 </section>
