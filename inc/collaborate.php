fcb9301da7b04090097c93061e3ec740f13812b1
Tom Brooks <tom@olospo.co.uk>
2025-02-28 14:03:31 +0000

    CTA fix

--- inc/collaborate.php
+++ inc/collaborate.php
@@ -1,7 +1,28 @@
+<?php 
+// Retrieve the call_to_action group from the options page.
+$cta = get_field('call_to_action', 'option');
+
+$cta_title = isset($cta['cta_title']) ? $cta['cta_title'] : '';
+$cta_description = isset($cta['cta_description']) ? $cta['cta_description'] : '';
+$cta_button  = isset($cta['cta_button']) ? $cta['cta_button'] : '';
+?>
+
 <section class="cta collaborate">
   <div class="container">
-    <h4>Collaborate with us.</h4>
-    <p>Have a project in mind? We’d love to hear about it.</p>
-    <a href="<?php echo get_site_url(); ?>/contact" class="button accent">Get in touch</a>
+    <h4><?php echo esc_html($cta_title); ?></h4>
+    <p><?php echo esc_html($cta_description); ?></p>
+    <?php if ( $cta_button ) {
+        if ( is_array( $cta_button ) ) {
+          $button_url    = isset($cta_button['url']) ? $cta_button['url'] : '';
+          $button_title  = isset($cta_button['title']) ? $cta_button['title'] : 'Get in touch';
+          $button_target = isset($cta_button['target']) ? $cta_button['target'] : '_self';
+        } else {
+          $button_url    = $cta_button;
+          $button_title  = 'Get in touch';
+          $button_target = '_self';
+        }
+      if ( $button_url ) : ?>
+      <a href="<?php echo esc_url($button_url); ?>" class="button accent" target="<?php echo esc_attr($button_target); ?>"><?php echo esc_html($button_title); ?></a>
+    <?php endif; } ?>
   </div>  
 </section>
