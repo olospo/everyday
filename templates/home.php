3be22a0d6797521278c74123e3443be0eed177fd
Tom Brooks <tom@olospo.co.uk>
2025-05-16 14:56:26 +0100

    Relative fix

--- templates/home.php
+++ templates/home.php
@@ -238,7 +238,7 @@ get_header(); ?>
           ?>
           <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
           <?php the_excerpt(); ?>
-          <span class="author"><?php echo get_field('authors'); ?></span> &#x25AA; <span class="date"><?php the_time("F j, Y"); ?></span>
+          <div class="meta"><span class="author"><?php echo get_field('authors'); ?></span> &#x25AA; <span class="date"><?php the_time("F j, Y"); ?></span></div>
           </div>
         </div>
       </article>
