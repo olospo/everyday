4621cf0d46e4a7e044c89c5b445d1a8ea333230b
Tom Brooks <tom@olospo.co.uk>
2025-05-20 12:45:42 +0100

    Contact template

--- templates/contact.php
+++ templates/contact.php
@@ -14,27 +14,11 @@ get_header(); ?>
 <section class="contact form">
   <div class="container">
     <div class="content one-half column">
-      <h2>How can we help?</h2>
-      <p>We’d love to learn more and discover what we can design together.</p>
-      <?php echo do_shortcode('[gravityform id="5" title="true"]'); ?>
-      <!-- <form action="#" method="POST" class="contact-form">
-        <label>Your name</label>
-        <input type="text" name="name" required>
-        <label>Email</label>
-        <input type="email" name="email" required>
-        <label>Company name</label>
-        <input type="text" name="company" required>
-        <label>How did you hear about us?</label>
-        <input type="text" name="hear">
-        <label>How can we work together?</label>
-        <textarea name="message" required></textarea>
-        <button type="submit">Submit</button>
-      </form> -->
+      <?php the_content(); ?>
     </div>
     <div class="bg one-half column"></div>
   </div>
 </section>
-
 
 <?php endwhile; // end of the loop. ?>
 
