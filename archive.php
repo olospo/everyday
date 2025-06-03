4cf372980f08c317c249e3aebb36b4593328bf06
Tom Brooks <tom@olospo.co.uk>
2025-06-03 15:58:06 +0100

    Large quote added

--- archive.php
+++ archive.php
@@ -56,11 +56,11 @@ get_header(); ?>
             ),
           ),
         );
-        $testimonial_query = new WP_Query( $testimonial_args );
-        $testimonials = $testimonial_query->posts;
-        $testimonial_count = count( $testimonials );
-        $testimonial_index = 0;
-        $displayed_testimonials = array(); // Array to track displayed testimonials
+        $testimonial_query  = new WP_Query( $testimonial_args );
+        $testimonials       = $testimonial_query->posts;
+        $testimonial_count  = count( $testimonials );
+        $testimonial_index  = 0;
+        $displayed_testimonials = array(); // Track which testimonial indexes have been shown
 
         // Loop through the Case Studies.
         $counter = 0;
@@ -69,21 +69,39 @@ get_header(); ?>
             $counter++;
             get_template_part('inc/work'); // Output the Case Study template
 
-            // After every third Case Study, output a Testimonial (if available and not displayed before)
+            // After every 2nd Case Study, output a Testimonial (if available and not already displayed)
             if ( $counter % 2 == 0 && $testimonial_count > 0 ) {
-              // Find a testimonial that has not been displayed before
-              while ($testimonial_index < $testimonial_count && in_array($testimonial_index, $displayed_testimonials)) {
-                $testimonial_index++; // Skip already displayed testimonials
+              // Find next testimonial that hasn't been displayed yet
+              while ( $testimonial_index < $testimonial_count && in_array( $testimonial_index, $displayed_testimonials ) ) {
+                $testimonial_index++;
               }
 
-              // If there are still available testimonials, display one
-              if ($testimonial_index < $testimonial_count) {
+              // If there is still a testimonial available, display it
+              if ( $testimonial_index < $testimonial_count ) {
                 $testimonial_post = $testimonials[ $testimonial_index ];
                 setup_postdata( $testimonial_post );
-                // Output the testimonial template
-                get_template_part('inc/work_quote', null, array('testimonial' => $testimonial_post));
+
+                // Determine which template to use:
+                // - On the 3rd time we're about to display a testimonial, use 'work_quote_large'
+                // - Otherwise, use the regular 'work_quote'
+                $next_display_count = count( $displayed_testimonials ) + 1;
+                if ( $next_display_count === 3 ) {
+                  get_template_part(
+                    'inc/work_quote_large',
+                    null,
+                    array( 'testimonial' => $testimonial_post )
+                  );
+                } else {
+                  get_template_part(
+                    'inc/work_quote',
+                    null,
+                    array( 'testimonial' => $testimonial_post )
+                  );
+                }
+
                 wp_reset_postdata();
-                $displayed_testimonials[] = $testimonial_index; // Mark the testimonial as displayed
+                // Mark this testimonial index as displayed
+                $displayed_testimonials[] = $testimonial_index;
                 $testimonial_index++;
               }
             }
@@ -95,7 +113,6 @@ get_header(); ?>
     </div>
   </div>
 </section>
-
 
 <?php get_template_part('inc/collaborate'); ?>
 
---
+++ inc/work_quote_large.php
@@ -0,0 +1,25 @@
+<?php
+// Check if the testimonial post was passed in.
+if ( isset($args['testimonial']) && is_object($args['testimonial']) ) {
+    $testimonial = $args['testimonial'];
+    $testimonial_id = $testimonial->ID;
+    $name = get_field('author_name', $testimonial_id);
+    $job_title = get_field('job_title', $testimonial_id);
+    // You can also use $testimonial->post_content for content.
+} else {
+    // Fallback if needed.
+    $name = '';
+    $job_title = '';
+}
+?>
+
+<article class="work_quote twelve columns">
+  <div class="content twelve columns">
+    <blockquote>
+      <?php echo apply_filters('the_content', $testimonial->post_content); ?>
+      <cite><?php echo esc_html($name); ?></cite><br />
+      <span><?php echo esc_html($job_title); ?></span>
+    </blockquote>
+  </div>
+</article>
+<article></article>
